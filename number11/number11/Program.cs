9234f2910cecd872b6045c2b9b6acd02a75b8165
Тимур <groboment@gmail.com>
2016-09-25 23:15:41 +0300

    ДЗ-1

---
+++ number11/number11/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace number11
+{
+    class Program
+    {
+        static string Subtrac(string a, string b)
+        {
+            // Вычитание длинной арифметики.
+            string s="";
+            return s;
+        }
+
+        static string Create(int f, int k)
+        {
+            // Образование строки-числа вида f*10^k.
+            string s = "";
+            return s;
+        }
+
+        static void Main(string[] args)
+        {
+            int n;
+            string a, b;
+            n = int.Parse(Console.ReadLine());
+            a = Create(495, 2 * n - 3);
+            b = Create(45, n - 2);
+            a = Subtrac(a, b);
+            Console.WriteLine("Сумма всех {0}-значных чисел={1}", n, a);
+        }
+    }
+}
