e331dcb0d45a79a57360961b8ef99af9f79102ef
Тимур <groboment@gmail.com>
2017-03-28 23:25:24 +0300

    ASD-1

--- ASD_Semesdtrovka_1/ASD_Semesdtrovka_1/Program.cs
+++ ASD_Semesdtrovka_1/ASD_Semesdtrovka_1/Program.cs
@@ -9,6 +9,10 @@ namespace ASD_Semesdtrovka_1
     class DNF
     {
         List<Konj> KList;
+        public DNF()
+        {
+            KList = new List<Konj>();
+        }
         public DNF(string s)
         {
             List<string> k = s.Split('V').ToList();
@@ -33,44 +37,66 @@ namespace ASD_Semesdtrovka_1
                 //}
                 //s.Remove(s.Length - 1, 1);
                 s.Append(i.ToString());
-                s.Append("V");
+                s.Append(" V ");
             }
-            s.Remove(s.Length - 1, 1);
+            s.Remove(s.Length - 3, 3);
             return s.ToString();
         }
-        public void Insert(Konj k)
+        public void Insert(Konj k,bool f=false)
         {
-            if (!KList.Contains(k)) // check
+            // Какая-то ересь. Метод работает прямо противоположно, если вызвать его напрямую, он добавит элемент если
+            // он уже есть в списке и наоборот, не добавит, если его нет. через метод Disj работает корректно.
+            // Пришлось запилить костыль-флаг.
+            // разобрался - нужна своя реализация contain!!!
+            if ((KList.Contains(k)==true)&(f==false)||(f==true)&(KList.Contains(k) == false)) // check
             {
                 KList.Add(k);
             }
+            //Console.WriteLine(KList[0].ToString().Equals(k.ToString()));
             return;
         }
-        public DNF Disj(DNF d)
+        public static DNF Disj(DNF a,DNF b)
         {
-            return d;
+            DNF w = new DNF();
+            w.KList = a.KList;
+            foreach(Konj i in b.KList)
+            {
+                w.Insert(i,true);
+            }
+            return w;
         }
-        public bool value(bool[] v)
+        public bool Value(bool[] a)
         {
-            return true;
+            bool f = false;
+            foreach(Konj i in KList)
+            {
+                f = f | i.Value(a);
+            }
+            return f; //!!!!
         }
         public void SortByLength()
         {
-
+            KList.Sort();
         }
-        public DNF DNFWith(int i)
+        public static DNF DNFWith(DNF a,int i)
         {
-            return this;
+            DNF w = new DNF();
+            w.KList = a.KList;
+            foreach(Konj q in w.KList)
+            {
+                q.AppendX(i);
+            }
+            return w;
         }
     }
-    class Konj : IEquatable<Konj>,IComparable<Konj>
+    class Konj : IComparable<Konj>,IEqualityComparer<Konj>
     {
         public List<int> ArLst;
         public Konj(string a)
         {
             ArLst = new List<int>();
             List<string> d = a.Split('&').ToList();
-            int y = 1;
+            //int y = 1;
             foreach (string j in d)
             {
                 if (j[0] == '-')
@@ -83,37 +109,112 @@ namespace ASD_Semesdtrovka_1
                 }
             }
         }
+        public bool Value(bool[] a)
+        {
+            bool f = true,d;
+            foreach(int i in ArLst)
+            {
+                if (i > 0)
+                {
+                    d = true;
+                }
+                else
+                {
+                    d = false;
+                }
+                if (!a[Math.Abs(i) - 1])
+                {
+                    if (d)
+                    {
+                        d = false;
+                    }
+                    else
+                    {
+                        d = true;
+                    }
+                }
+                f = f & d;
+                if (!f)
+                {
+                    return false;
+                }
+            }
+            return f;
+        }
+        public void AppendX(int i)
+        {
+            if (!ArLst.Contains(Math.Abs(i)))
+            {
+                ArLst.Add(i);
+            }
+        }
 
         public int CompareTo(Konj other)
         {
             return ToString().Length.CompareTo(other.ToString().Length); // если сравнивает не в ту сторону - прерставить экземпляры.
         }
 
-        public bool Equals(Konj other)
-        {
-            //throw new NotImplementedException();
-            return Equals(ArLst, other.ArLst);
-        }
+        //public bool Equals(Konj other)
+        //{
+        //    //throw new NotImplementedException();
+        //    return Equals(ArLst, other.ArLst);
+        //}
         public override string ToString()
         {
             StringBuilder s = new StringBuilder();
 
             foreach (int j in ArLst)
             {
-                if (j > 0)
-                {
-                    s.AppendFormat($"{0}X{1}&", j > 0 ? "" : "-", Math.Abs(j)); // чекнуть
-                }
+                //if (j > 0)
+                //{
+                    //string r = j > 0 ? "" : "-";
+                //Console.WriteLine(r);
+                    s.AppendFormat(String.Format("{0}X{1}&", j > 0 ? "" : "-", Math.Abs(j))); // чекнуть
+                //}
             }
             s.Remove(s.Length - 1, 1);
             return s.ToString();
+        }
+
+        public bool Equals(Konj x, Konj y)
+        {
+            //throw new NotImplementedException();
+            return Equals(x.ToString(), y.ToString());
+            //return x.ToString().Equals(y.ToString());
+        }
+
+        public int GetHashCode(Konj obj)
+        {
+            //throw new NotImplementedException();
+            return obj.ToString().GetHashCode();
         }
     }
     class Program
     {
         static void Main(string[] args)
         {
-//в процессе разработки
+            // реализовать contain для insert!!!
+            DNF a = new DNF(Console.ReadLine());
+            DNF b = new DNF(Console.ReadLine());
+            Console.WriteLine(a);
+            Console.WriteLine("Check insert");
+            a.Insert(new Konj(Console.ReadLine()));
+            Console.WriteLine(a);
+            Console.WriteLine("Check disjunc");
+            Console.WriteLine(DNF.Disj(a, b));
+            Console.WriteLine("Check calculate");
+            bool[] c = new bool[5];
+            for(int i=0;i<5;i++)
+            {
+                c[i] = bool.Parse(Console.ReadLine());
+            }
+            Console.WriteLine(a.Value(c));
+            Console.WriteLine("Check sort");
+            a.SortByLength();
+            Console.WriteLine(a);
+            Console.WriteLine("Check dnfWith");
+            Console.WriteLine(DNF.DNFWith(a, int.Parse(Console.ReadLine())));
+            Console.ReadLine();
         }
     }
 }
